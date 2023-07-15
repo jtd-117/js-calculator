dab4b31ef15d6d9c77c04658647ea4b9562b9f35
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-07-15 14:56:26 +1000

    Add script.js file docs

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * @file        script.js
+ * @description Implements logic for calculator
+ * @author      Jude Thaddeau Data
+ * @link        GitHub: https://github.com/jtd-117
+* --------------------------------------------------------------------------- */
+
+
+
+/* -------------------------------------------------------------------------- */
+
