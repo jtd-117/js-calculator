80a6580067441609e455dd939889f0cfbfd4d39e
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-07-17 15:56:10 +1000

    Define 'add', 'subtract', 'multiply' & 'divide' functions

--- script.js
+++ script.js
@@ -14,27 +14,87 @@ const operators = Object.freeze({
     divide:     3,
 });
 /* -------------------------------------------------------------------------- */
+/**
+ * @description     
+ * @param {number}  value1 The first numeric string
+ * @param {number}  value2 The second numeric string
+ */
+function add(value1, value2) {
+    
+    // STEP 1: Convert arguments into values
+    let number1 = Number(value1);
+    let number2 = Number(value2);
 
-function add(value1, value2);
+    // STEP 2: Add the numbers
+    let result = number1 + number2;
 
+    // STEP 3: Return the result as a string
+    return result.toString();
+}
 /* -------------------------------------------------------------------------- */
+/**
+ * @description     
+ * @param {number}  value1 The first numeric string
+ * @param {number}  value2 The second numeric string
+ */
+function subtract(value1, value2) {
 
-function subtract(value1, value2);
+    // STEP 1: Convert arguments into values
+    let number1 = Number(value1);
+    let number2 = Number(value2);
 
+    // STEP 2: Subtract the numbers
+    let result = number1 - number2;
+
+    // STEP 3: Return the result as a string
+    return result.toString();
+}
 /* -------------------------------------------------------------------------- */
+/**
+ * @description     
+ * @param {number}  value1 The first numeric string
+ * @param {number}  value2 The second numeric string
+ */
+function multiply(value1, value2) {
 
-function multiply(value1, value2);
+    // STEP 1: Convert arguments into values
+    let number1 = Number(value1);
+    let number2 = Number(value2);
 
+    // STEP 2: Multiply the numbers
+    let result = number1 * number2;
+
+    // STEP 3: Return the result as a string
+    return result.toString();
+}
 /* -------------------------------------------------------------------------- */
+/**
+ * @description     
+ * @param {number}  value1 The first numeric string
+ * @param {number}  value2 The second numeric string
+ */
+function divide(value1, value2) {
 
-function divide(value1, value2);
+    // STEP 1: Convert arguments into values
+    let number1 = Number(value1);
+    let number2 = Number(value2);
 
+    // STEP 2: Divide the numbers
+    let result = number1 / number2;
+
+    // STEP 3: Return the result as a string
+    return result.toString();
+}
 /* -------------------------------------------------------------------------- */
-
+/**
+ * @description     
+ * @param {number}  value1 The first numeric string
+ * @param {number}  value2 The second numeric string
+ */
 function operate(value1, value2);
 
 /* -------------------------------------------------------------------------- */
 
-let lhsValue = null;
-let rhsValue = null;
-let operator = null;
+let lhsValue = "";
+let rhsValue = "";
+let operator = "";
