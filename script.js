1d47d5ebe156a0081da6aa1ef91265d8fb13168e
Jude Thaddeau Data <78249269+jtd-117@users.noreply.github.com>
2023-07-17 15:22:57 +1000

    Add 'operators' enum

--- script.js
+++ script.js
@@ -4,8 +4,37 @@
  * @author      Jude Thaddeau Data
  * @link        GitHub: https://github.com/jtd-117
 * --------------------------------------------------------------------------- */
+/**
+ * @description An enumeration of operations supported by the calculator.
+ */
+const operators = Object.freeze({
+    add:        0,
+    subtract:   1,
+    multiply:   2,
+    divide:     3,
+});
+/* -------------------------------------------------------------------------- */
 
-
+function add(value1, value2);
 
 /* -------------------------------------------------------------------------- */
 
+function subtract(value1, value2);
+
+/* -------------------------------------------------------------------------- */
+
+function multiply(value1, value2);
+
+/* -------------------------------------------------------------------------- */
+
+function divide(value1, value2);
+
+/* -------------------------------------------------------------------------- */
+
+function operate(value1, value2);
+
+/* -------------------------------------------------------------------------- */
+
+let lhsValue = null;
+let rhsValue = null;
+let operator = null;
